6ed4a6f12c11799cdf5f3eb62beeb60b7f6ee243
Poovarasan955 <23203016@srcas.ac.in>
2024-07-01 11:58:24 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample{
+  public static void main(string[]args)
+  {
+    system.out.println("Hello");
+    system.out.println("Welcome");
+     system.out.println("to");
+     system.out.println("Deveops");
+     system.out.println("class");
+  }
